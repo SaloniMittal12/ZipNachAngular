e7ae5b3a6e835ea95c01071cc11d9dbb29d6fa49
SaloniMittal1206 <saloni@i-ace.in>
2019-02-23 17:49:58 +0530

    Login Page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../navbar.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( public nav: NavbarService ) {}
 
   ngOnInit() {
   }
