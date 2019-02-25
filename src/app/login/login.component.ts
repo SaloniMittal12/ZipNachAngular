478cc9443d2ec97b91ba7a9589dc6f29eaf6c1cd
aarati_vashisht <aarati.vashisht2804@gmail.com>
2019-02-25 17:40:42 +0530

    changes in login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit,Attribute} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NavbarService } from '../navbar.service';
-
+import { FormGroup, FormControl,  Validators , FormBuilder} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(public nav: NavbarService) { }
+  loginForm: FormGroup;
+  submitted = false;
+  constructor(public nav: NavbarService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
    this.nav.hide();
+   this.loginForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+});
 
   }
+  onSubmit() {
+    this.submitted = true;
 
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value));
+}
+get f() { return this.loginForm.controls; }
 }
